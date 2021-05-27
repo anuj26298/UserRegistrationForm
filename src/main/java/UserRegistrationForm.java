82f115d519eb1019df5f0fcad421711952b32d09
anuj26298 <kumar26298anuj@gmail.com>
2021-05-27 17:12:09 +0530

    ADDED validate last name feature

--- src/main/java/UserRegistrationForm.java
+++ src/main/java/UserRegistrationForm.java
@@ -3,17 +3,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationForm {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserRegistrationForm registrationform = new UserRegistrationForm();
+        boolean isValidName;
 
         System.out.println("--++-- User Registration --++--");
         System.out.println("Enter your Details below:");
 
         System.out.println("First Name: ");
         String firstName = scanner.nextLine();
+        isValidName = registrationform.usersValidFirstName(firstName);
+        System.out.println(isValidName);
 
-        boolean isValidName = registrationform.usersValidFirstName(firstName);
+        System.out.println("Last Name: ");
+        String lastName = scanner.nextLine();
+        isValidName = registrationform.usersValidLastName(lastName);
         System.out.println(isValidName);
 
     }
@@ -23,6 +29,14 @@ public class UserRegistrationForm {
 
         Pattern pattern = Pattern.compile(regEx);
         Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    private static boolean usersValidLastName(String lastName){
+        String regEx = "^[A-Z][a-z]{3,16}";
+
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
     }
 }
